1de525cf007d7c370114f4844b6c688e22786602
Лейла Исмаилова <Trupeshnitzia@gmail.com>
2022-10-02 16:36:17 +0300

    получен результат

--- Program.cs
+++ Program.cs
@@ -4,4 +4,26 @@
 
 82 -> 10
 
-9012 -> 12*/
+9012 -> 12
+
+*/
+
+Console.Clear();  		
+
+int UserNumber = 0;
+int sum = 0;	
+
+Console.WriteLine("Введите  число " );	 
+UserNumber = Convert.ToInt32(Console.ReadLine());
+
+
+	  	  
+while (UserNumber > 0)
+
+        {
+	        sum = sum + UserNumber % 10;
+	        UserNumber = UserNumber / 10;
+	    }	
+    
+    Console.WriteLine($" Сумма цифр числа  {sum}");
+
